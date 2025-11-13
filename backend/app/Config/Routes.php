bc1f63a35b06220a3ede3f951b396d5d1467ea3c
User@LAPTOP-CMEFHP1R <justinkirkendozo@gmail.com>
2025-11-13 17:52:26 +0800

    feat(frontend/login page): Added a new login page
    - added new `view`
    - update new `controller`
    - updated routes to route the login page

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Users::showLandingPage');
+
+// Auth page
+$routes->get('/login', 'Auth::showLoginPage');
