61502c07f8655dbd0af3af9a5fa5547d3094c942
User@LAPTOP-CMEFHP1R <justinkirkendozo@gmail.com>
2025-11-13 18:03:21 +0800

    feat(frontend/moodboardPage): Add moodboard page and update routes

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Users::showLandingPage');
+$routes->get('/moodboard', 'Users::showMoodboard');
 
 // Auth page
 $routes->get('/login', 'Auth::showLoginPage');
