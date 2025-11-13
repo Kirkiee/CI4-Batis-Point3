ca41e618756fbaf7df51460f17181bf95d16d704
User@LAPTOP-CMEFHP1R <justinkirkendozo@gmail.com>
2025-11-13 18:48:39 +0800

    refactor(auth): Updated Auth Controller with following functions and expose Routes
    - Login = Auth::login
    - Logout = Auth::logout
    - Sign Up = Auth::signup

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -12,3 +12,29 @@ $routes->get('/roadmap', 'Users::showRoadmap');
 // Auth page
 $routes->get('/login', 'Auth::showLoginPage');
 $routes->get('/signup', 'Auth::showSignupPage');
+
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+// Public pages
+$routes->get('/', 'Users::showLandingPage');
+$routes->get('/moodboard', 'Users::showMoodboard');
+$routes->get('/roadmap', 'Users::showRoadmap');
+
+// Auth page
+$routes->get('/login', 'Auth::showLoginPage');
+$routes->get('/signup', 'Auth::showSignupPage');
+
+// Auth actions (Employees Only)
+$routes->post('/login', 'Auth::login');
+$routes->post('/logout', 'Auth::logout');
+$routes->post('/signup', 'Auth::signup');
+
+// Admin pages
+$routes->get('/admin/dashboard', 'Admin::showDashboard');
+
